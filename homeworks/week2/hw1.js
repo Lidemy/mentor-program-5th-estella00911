970d1200838b90be4b747abaec84bcc0ca1730fb
estella00911 <estella00911@gmail.com>
2021-04-25 10:44:27 +0800

    final ver week2 hw

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,17 +1,8 @@
 function printStars(n) {
-  var output = '*';     //initial string
-  var iterate = '\n*';  //add string
-  //[if] check n=1 or n!=1
-  if (n > 1) {
-    for (let i = 0; i < n - 1; i++) {  //for loop to add '\n*'
-      output += iterate;
-    }  //endfor
-    console.log(output);
-  }//endif
-  else if (n == 1) {   //
-    console.log(output);
-  }//end elseif
-}//end function
-
-
+  if (n >= 1 && n <= 30) {
+    for (let i = 1; i <= n; i++) {
+      console.log('*');
+    }
+  }
+}
 printStars(3)
