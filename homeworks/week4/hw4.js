b9d3ebf2bb0abc0158eecb1c5f7ac1c305dfb5e8
estella00911 <estella00911@gmail.com>
2021-05-06 15:54:37 +0800

    完成w4hw4

---
+++ homeworks/week4/hw4.js
@@ -0,0 +1,19 @@
+const request = require('request')
+
+request.get(
+  {
+    url: 'https://api.twitch.tv/kraken/games/top?_limit=100',
+    headers: {
+      accept: 'application/vnd.twitchtv.v5+json',
+      'client-id': 'bmrryhslbf6vtr71xzyluf4ahb641o'
+    }
+  },
+  (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const json = JSON.parse(body)
+      for (let i = 0; i < json.top.length; i++) {
+        console.log(json.top[i].viewers, json.top[i].game.name) // json.top[0] object
+      }
+    }
+  }
+)
