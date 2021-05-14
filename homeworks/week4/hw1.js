40584594a5f6285854fa65f84e3d72dca325f0e9
estella00911 <estella00911@gmail.com>
2021-05-14 14:46:35 +0800

    增加try catch預期發生的錯誤回應

--- homeworks/week4/hw1.js
+++ homeworks/week4/hw1.js
@@ -1,16 +1,24 @@
 const request = require('request')
 
 request(
-  'https://lidemy-book-store.herokuapp.com/books',
+  'https://lidemy-book-store.herokuapp.com/books', // correct API link
+  // 'https://lidemy-http-challenge.herokuapp.com/api/v2', // 需 authorization
+  // 'https://lidemy-book-store.herokuappsfdsfs.com/books', // incorrect API link
   (error, response, body) => {
-  // console.log(typeof(body)) // string
+    if (error) {
+      console.log('伺服器無回應（以下為可能解決辦法）:') // https://support.freshdesk.com/support/discussions/topics/317310
+      console.log('The domain that you are trying to reach is wrong or unavailable.\nPlease check the URL that you are trying to request is correct.\n', 'error\n', error)
+      return
+    }
+    let json = {}
     try {
-      const json = JSON.parse(body) // object （json格式）
-      for (let i = 0; i < 10; i++) {
-        console.log(json[i].id, json[i].name)
-      }
+      json = JSON.parse(body) // object （json格式）
     } catch (error) {
-      console.log(error)
+      console.log('error!!! 伺服器有回應，但是 API 文件內的資料不符合 JSON 格式')
+      return
+    }
+    for (let i = 0; i < 10; i++) {
+      console.log(`ID=${json[i].id} ${json[i].name}`)
     }
   }
 )
