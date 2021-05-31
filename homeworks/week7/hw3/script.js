4c16183c6ace2ed775bebe7a72cecd1aba5ab26f
estella00911 <estella00911@gmail.com>
2021-05-31 12:17:38 +0800

    w7 hw3 新增RWD與修改新增鈕

--- homeworks/week7/hw3/script.js
+++ homeworks/week7/hw3/script.js
@@ -1,12 +1,12 @@
 let number = 0
 document.querySelector('.container').addEventListener('click', (e) => {
   if (e.target.classList.contains('btn-add')) {
-    // const inputWords = document.querySelector('input[name=input-new]') // value = inputWords.value
+    const inputWords = document.querySelector('input[name=input-new]') // value = inputWords.value
     const divAdd = document.createElement('div')
     divAdd.classList.add('todo__list')
     divAdd.setAttribute('id', `${number}`)
     divAdd.setAttribute('οnmοusemοve', 'changeBgColor()')
-    divAdd.innerHTML = `<div><label class='line-through'><input type='checkbox' class='btn-checkbox' name='btn-name'></input><span>待辦事項！</span></label></div>
+    divAdd.innerHTML = `<div><label class='line-through'><input type='checkbox' class='btn-checkbox' name='btn-name'></input><span>${inputWords.value}</span></label></div>
 <div><button class='btn-delete'>刪除</button></div>`
     document.querySelector('.section__todo-lists').appendChild(divAdd)
     number += 1
