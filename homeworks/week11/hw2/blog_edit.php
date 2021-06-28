d4bca71cef8022fa29df903dcf5713f9d8860f35
estella00911 <estella00911@gmail.com>
2021-06-28 10:24:43 +0800

    修改ckEditor 由 CDN 引入

--- homeworks/week11/hw2/blog_edit.php
+++ homeworks/week11/hw2/blog_edit.php
@@ -9,7 +9,7 @@
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Berkshire+Swash&family=Caveat:wght@700&family=Gloria+Hallelujah&family=Gochi+Hand&family=Knewave&family=Lemonada:wght@600&family=Merriweather:ital@1&family=Noto+Sans+TC&family=Noto+Serif+TC&family=Permanent+Marker&family=Special+Elite&family=Noto+Sans&display=swap" rel="stylesheet">
 <script src="../../../node_modules/@ckeditor/ckeditor5-build-classic/build/ckeditor.js"></script>
-<!-- ../../../ckeditor5packages/ckeditor5-build-classic/src/ckeditor.js -->
+<script src="https://cdn.ckeditor.com/ckeditor5/28.0.0/classic/ckeditor.js"></script>
 </head>
 
 <body>
