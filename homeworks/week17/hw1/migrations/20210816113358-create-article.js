a7759bc57b9444e5e9e897fbe142812d0953effb
estella00911 <estella00911@gmail.com>
2021-09-14 12:22:34 +0800

    重新提交 w17 作業

---
+++ homeworks/week17/hw1/migrations/20210816113358-create-article.js
@@ -0,0 +1,39 @@
+
+module.exports = {
+  up: async(queryInterface, Sequelize) => {
+    await queryInterface.createTable('Articles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      category: {
+        type: Sequelize.STRING
+      },
+      content: {
+        type: Sequelize.TEXT
+      },
+      is_deleted: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      UserId: {
+        type: Sequelize.INTEGER
+      }
+    })
+  },
+  down: async(queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Articles')
+  }
+}
